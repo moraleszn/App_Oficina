ec8c19d5b96a2206e8818a237fd2968cc0a429e2
moraleszn <diegomorales.tecno@gmail.com>
2026-01-12 23:16:06 -0300

    Refactor templates and styles: remove unused files, update base layout, and enhance sidebar functionality

--- clientes/views.py
+++ clientes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'clientes/base.html')
+    return render(request, 'clientes.html')
 
 def clientes(request):\
     return render(request, 'clientes.html')
