727067d3830b242225e36a4a49a7563dc5a66cba
moraleszn <diegomorales.tecno@gmail.com>
2026-01-27 21:06:37 -0300

    Add Cliente and Carro models, update views and templates for client registration

--- clientes/views.py
+++ clientes/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Carro
 
 def index(request):
     return render(request, 'clientes.html')
 
-def clientes(request):\
-    return render(request, 'clientes.html')
+def clientes(request):
+    if request.method == 'GET':
+        return render(request, 'clientes.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+
+        cliente.save()
+        
+        x = list(zip(carros, placas, anos))
+
+        print(x)
